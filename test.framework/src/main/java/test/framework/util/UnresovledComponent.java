9fa1927248db3f1362260d5696cd7f2b81df7081
elmar.chen <elmar.chen@H2300-0563L.perficient.com>
2018-05-05 22:29:19 +0800

    half done

--- test.framework/src/main/java/test/framework/util/UnresovledComponent.java
+++ test.framework/src/main/java/test/framework/util/UnresovledComponent.java
@@ -1,5 +1,42 @@
 package test.framework.util;
 
 public class UnresovledComponent {
-
+	private Class<?> clazz;
+	private String pattern;
+	private String patternKey;
+	private int patternIdx;
+	private ASTNode astNode;
+	public Class<?> getClazz() {
+		return clazz;
+	}
+	public void setClazz(Class<?> clazz) {
+		this.clazz = clazz;
+	}
+	public String getPattern() {
+		return pattern;
+	}
+	public void setPattern(String pattern) {
+		this.pattern = pattern;
+	}
+	public String getPatternKey() {
+		return patternKey;
+	}
+	public void setPatternKey(String patternKey) {
+		this.patternKey = patternKey;
+	}
+	
+	public int getPatternIdx() {
+		return patternIdx;
+	}
+	public void setPatternIdx(int patternIdx) {
+		this.patternIdx = patternIdx;
+	}
+	public ASTNode getAstNode() {
+		return astNode;
+	}
+	public void setAstNode(ASTNode astNode) {
+		this.astNode = astNode;
+	}
+	
+	
 }
