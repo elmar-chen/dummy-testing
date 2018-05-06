9530206e7446e9dc8c25375fa9d5b9076c4880a1
elmar <chxiao@gmail.com>
2018-05-07 00:54:15 +0800

    sny

---
+++ test.framework/src/main/java/test/framework/util/PatternUtils.java
@@ -0,0 +1,9 @@
+package test.framework.util;
+
+import test.framework.util.ASTNode.NodeType;
+
+public class PatternUtils {
+	public static boolean isStatic(ASTNode.NodeType type) {
+		return type==NodeType.DEL || type==NodeType.TEXT;
+	}
+}
