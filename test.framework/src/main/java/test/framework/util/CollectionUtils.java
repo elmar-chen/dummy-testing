3ae6c89b40f07d4444d288844e46b0cb8828472a
elmar.chen <elmar.chen chxiao@gmail.com>
2018-05-08 18:32:43 +0800

    : Code refactor/cleanup/unittest
    Task-Url:

--- test.framework/src/main/java/test/framework/util/CollectionUtils.java
+++ test.framework/src/main/java/test/framework/util/CollectionUtils.java
@@ -2,19 +2,20 @@ package test.framework.util;
 
 import java.util.Arrays;
 
-public class CollectionUtils{
-	
+public class CollectionUtils {
+
 	public static <T> int indexOf(Iterable<T> iterable, T obj) {
- 		int i = -1;
- 		for (T t : iterable) {
- 			i++;
-			if((obj==null && t==null) || t.equals(obj)) {
+		int i = -1;
+		for (T t : iterable) {
+			i++;
+			if ((obj == null && t == null) || t.equals(obj)) {
 				return i;
 			}
 		}
- 		return -1;
- 	};
- 	public static <T> int indexOf(T[] arr, T obj) {
- 		return indexOf(Arrays.asList(arr), obj);
- 	};
+		return -1;
+	};
+
+	public static <T> int indexOf(T[] arr, T obj) {
+		return indexOf(Arrays.asList(arr), obj);
+	};
 }
